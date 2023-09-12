99a24bf9f300f4895521ca54f1f2558743b9e1ba
Walter Tronics <waltertronics02@gmail.com>
2023-09-12 14:49:58 +0100

    add inintial code for the homepage

---
+++ components/home.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+
+const Home = () => {
+
+
+    return(
+        <>
+            <div>This is the homepage</div>
+        </>
+    )
+}
+
+export default Home
