95abc70c413a0b7ece5178f13ef0072f95f9ce8b
Walter Tronics <waltertronics02@gmail.com>
2023-09-12 15:53:50 +0100

    set routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import axios from 'axios'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/home'
 
 const App = () => {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   )
 }
