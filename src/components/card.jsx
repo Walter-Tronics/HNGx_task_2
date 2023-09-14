9cc395f8cb58e68be0bcfc28db9f8cade8bddfc1
Walter Tronics <waltertronics02@gmail.com>
2023-09-14 15:11:27 +0100

    add base working of and display of the card component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+import { Link } from "react-router-dom"
 
 
-const Card = () => {
+const Card = ({link, id, src, release, title}) => {
+    const [fav, setFav] = useState(false);
 
+    function addToFav(e,id) {
+        setFav(!fav);
+        e.stopPropagation();
+    }
 
     return (
         <>
-            <div>This is your card</div>
+            <Link to={link} className="card">
+                <i onClick={()=> addToFav(id)} className={`fa-solid fa-heart ${fav ? 'fav-icon' : 'fav'}`}></i>
+                <div className="poster">
+                    <img src={src}/>
+                </div>
+
+                <div className="card_info">
+                    <span>{release}</span>
+                    <h2>{title}</h2>
+                </div>
+            </Link>
         </>
     )
 }
