6d53f4dccdfe2c3959a4ba4ff88506f36200fa39
Walter Tronics <waltertronics02@gmail.com>
2023-09-15 14:03:42 +0100

    move the configuration for api call to the context file

--- src/context.jsx
+++ src/context.jsx
@@ -15,13 +15,19 @@ getQuery();
 
 export const MovieProvider = ({children}) => {
     const [query, setQuery] = useState(queryInfo);
+
+    const BearerToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YWFlNTgyMmM2NTM3ZWNjMGJkNDE5ZDg3OWJhYWRhOCIsInN1YiI6IjY0ZmYyMTliZWIxNGZhMDBlMzA3NmM3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R3LifTPiTZk63tbJHatj3dD4ZvUoi-bb7whU20DCrJY";
+
+    const config = {
+    headers: { Authorization: `Bearer ${BearerToken}` },
+    };
     
     useEffect(()=>{
         localStorage.setItem("query", query);
     }, [query]);
 
     return (
-        <MovieContext.Provider value={{ query, setQuery }}>
+        <MovieContext.Provider value={{ query, setQuery, config }}>
             {children}
         </MovieContext.Provider>
     )
