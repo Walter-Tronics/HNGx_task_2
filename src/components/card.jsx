f530cdb4ea277ae77c3b41e009f1d1d11c400301
Walter Tronics <waltertronics02@gmail.com>
2023-09-15 13:42:24 +0100

    try to stop event propagation

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,18 +2,22 @@ import { useState } from "react";
 import { Link } from "react-router-dom"
 
 
-const Card = ({link, id, src, release, title}) => {
+const Card = ({id, src, release, title}) => {
     const [fav, setFav] = useState(false);
 
-    function addToFav(e,id) {
+    function addToFav() {
         setFav(!fav);
-        e.stopPropagation();
     }
 
     return (
         <>
-            <Link to={link} className="card">
-                <i onClick={()=> addToFav(id)} className={`fa-solid fa-heart ${fav ? 'fav-icon' : 'fav'}`}></i>
+            <Link to={`/movies/${id}`} className="card">
+                <i onClick={e => {
+                e.stopPropagation(); 
+                addToFav();
+                }} 
+                className={`fa-solid fa-heart ${fav ? 'fav-icon' : 'fav'}`}></i>
+
                 <div className="poster">
                     <img src={src}/>
                 </div>
